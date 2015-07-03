07a675740c9222f50cdb56229cc711d543b34e76
drobune <s.yuichi227@gmail.com>
2015-07-03 14:54:51 +0900

    add goluent.go

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # goluent
 Override standard log library by using fluend-logger https://github.com/fluent/fluent-logger-golang
+
+# How to use
+
+in your *.go file
+```
+import {
+       log "github.com/drobune/goluent"
+}
+```
+
---
+++ goluent.go
@@ -0,0 +1,98 @@
+package goluent
+
+import (
+	"fmt"
+	"github.com/fluent/fluent-logger-golang/fluent"
+	stdLog "log"
+	"os"
+)
+
+const (
+	infoLog severity = iota
+	warningLog
+	errorLog
+	fatalLog
+	numSeverity = 4
+)
+
+var severityName = []string{
+	infoLog:    "INFO",
+	warningLog: "WARNING",
+	errorLog:   "ERROR",
+	fatalLog:   "FATAL",
+}
+
+var hostname string
+
+type severity int8
+
+func print(s severity, args ...interface{}) {
+	//connect fluent server
+	f, err := fluent.New(fluent.Config{
+		FluentPort: 24224,
+		FluentHost: "localhost",
+		TagPrefix:  "goluent." + getHostname(),
+	})
+
+	message := fmt.Sprint(args...)
+
+	if err == nil {
+		f.Post(severityName[s], map[string]string{"message": message})
+		stdLog.Println(message)
+	}
+}
+
+func printf(s severity, format string, args ...interface{}) {
+	//connect fluent server
+	f, err := fluent.New(fluent.Config{
+		FluentPort: 24224,
+		FluentHost: "localhost",
+		TagPrefix:  "goluent." + getHostname(),
+	})
+
+	message := fmt.Sprintf(format, args...)
+
+	if err == nil {
+		f.Post(severityName[s], map[string]string{"message": message})
+		stdLog.Println(message)
+	}
+}
+
+func Info(args ...interface{}) {
+	print(infoLog, args...)
+}
+
+func Infof(format string, args ...interface{}) {
+	printf(infoLog, format, args...)
+}
+
+func Warning(args ...interface{}) {
+	print(warningLog, args...)
+}
+
+func Warningf(format string, args ...interface{}) {
+	printf(warningLog, format, args...)
+}
+
+func Error(args ...interface{}) {
+	print(errorLog, args...)
+}
+
+func Errorf(format string, args ...interface{}) {
+	printf(errorLog, format, args...)
+}
+
+func Fatal(args ...interface{}) {
+	print(fatalLog, args...)
+}
+
+func Fatalf(format string, args ...interface{}) {
+	printf(fatalLog, format, args...)
+}
+
+func getHostname() string {
+	if hostname == "" {
+		hostname, _ = os.Hostname()
+	}
+	return hostname
+}
